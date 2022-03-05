2f81ab9f8dace6ace5f3ccdd821570754182364a
Bruno Trindade <brunotrndd@gmail.com>
2022-03-05 16:22:16 -0300

    Impl connection e user model

---
+++ src/interfaces/CreatedUser.ts
@@ -0,0 +1,9 @@
+export interface CreatedUser {
+  id: number;
+  
+  username: string;
+
+  classe: string;
+
+  level: number;
+}
---
+++ src/models/User.ts
@@ -0,0 +1,41 @@
+import { OkPacket } from 'mysql2';
+import { CreatedUser } from '../interfaces/CreatedUser';
+import connection from './connection';
+
+class User {
+  username: string;
+
+  classe: string;
+
+  level: number;
+
+  password: string;
+
+  constructor(username: string, classe: string, level: number, password: string) {
+    this.username = username;
+    this.classe = classe;
+    this.level = level;
+    this.password = password;
+  }
+
+  public async create(): Promise<CreatedUser> {
+    const [rows] = await connection.execute<OkPacket>(
+      'INSERT INTO products (username, classe, level, password) VALUES (?, ?, ?, ?)',
+      [this.username, this.classe, this.level, this.password],
+    );
+    const createdUser = {
+      id: rows.insertId,
+      username: this.username,
+      classe: this.classe,
+      level: this.level,
+    };
+    return createdUser;
+  }
+
+  // public static async getAll(): Promise<User> {
+  //   const [rows] = await connection.execute<RowDataPacket[]>('SELECT * FROM products');
+  //   return rows as User[];
+  // }
+}
+
+export default User;
