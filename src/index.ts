6966242a8b8e860bc28a5ba0743aea4799c3c4cc
Bruno Trindade <brunotrndd@gmail.com>
2022-03-06 13:05:26 -0300

    Ajuste app port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
